421f0e96661ebdf9265c89f84e8a7d69ed7fd1e4
Youness-Ailal <itsyounessaylal@gmail.com>
2023-10-22 20:17:50 +0100

    v 1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
-
 import partytown from "@astrojs/partytown";
 
-// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-  integrations: [compress({
-    html: true,
-    js: true
-  }), partytown()]
+  site: "https://kitilab.netlify.app/",
+  integrations: [
+    compress({
+      html: true,
+      js: true,
+    }),
+    partytown(),
+    sitemap(),
+  ],
 });
